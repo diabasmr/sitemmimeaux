7dff5c48fa6c1ae2d90211c269099a8fed6f2180
diabasmr <diaba.samoura@hotmail.com>
2025-06-12 15:33:34 +0200

    résolution problème d'affichage header page materiels

--- PHP/materiels.php
+++ PHP/materiels.php
@@ -105,9 +105,26 @@ include("../PHPpure/entete.php");
             <div class="mt-5 mt-md-auto d-flex align-items-center justify-content-md-between">
                 <h3 class="fs-1 fw-bold w-50 w-md-auto">Tous</h3>
                 <div class="d-flex justify-content-between align-items-center w-md-100 gap-3">
-                    <input type="search" placeholder="Rechercher" class="form-control w-100 w-md-50 p-3 search-input">
-                    <button class="search-btn btn p-3"><img src="../res/search.svg" alt="search"></button>
+                    <form method="post" class="d-flex" role="search">
+                        <input name='motcle' type="search" placeholder="Rechercher" class="form-control w-100 w-md-50 p-3 search-input">
+                        <button name="search" type="submit" class="search-btn btn ms-3 p-3"><img src="../res/search.svg" alt="search"></button>
+                    </form>
                 </div>
+                
+                <?php
+                $materiaux = []; 
+                if (isset($_POST['search'])) {
+                    $motcle = '%' . $_POST['motcle'] . '%';
+                
+                    $stmt = $pdo->prepare("SELECT * FROM materiel WHERE designation LIKE ? OR typeM LIKE ? OR descriptif LIKE ?");
+                    $stmt->execute([$motcle, $motcle, $motcle]);
+                
+                    $materiaux = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                } else {
+                    $stmt = $pdo->query("SELECT * FROM materiel");
+                    $materiaux = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                }
+                ?>
             </div>
             <!-- flex wrap qui change de 3 en 2 en 1 en dessous de 768px -->
             <div class="row justify-content-start align-items-baseline g-5 ">
@@ -189,52 +206,52 @@ include("../PHPpure/entete.php");
                 </div> -->
                 <?php
                 require_once("../PHPpure/connexion.php");
-                // recuperer les img dans ../materiel/[photop]
-                $sql = "SELECT * FROM materiel";
-                $result = $pdo->query($sql);
-                while ($row = $result->fetch()) {
-                    echo "<div class='col-6 col-md-4 d-flex justify-content-center align-items-center flex-column position-relative'>";
+                ?>
+                <?php foreach ($materiaux as $materiel): ?>
+                    <?php
+                    echo "<div class='materielItem col-6 col-md-4 d-flex justify-content-center align-items-center flex-column position-relative'>";
                     echo "<div class='position-absolute top-0 end-0 d-flex justify-content-between align-items-center p-4'>";
                     echo "<button class='btn bg-transparent border-0'>";
                     $sql1 = "SELECT * FROM favori_materiel WHERE idM = ? AND id = ?";
                     // avec idM = $row['idM'] et id = $_SESSION['user']['id']
                     $result1 = $pdo->prepare($sql1);
-                    $result1->execute([$row['idM'], $_SESSION['user']['id']]);
+                    $result1->execute([$materiel['idM'], $_SESSION['user']['id']]);
                     if ($result1->fetch()) {
-                        echo "<img src='../res/heartPlein.svg' alt='favory' onclick='retirerFavoris(" . $row['idM'] . ")'>";
+                        echo "<img src='../res/heartPlein.svg' alt='favory' onclick='retirerFavoris(" . $materiel['idM'] . ")'>";
                     } else {
-                        echo "<img src='../res/heartVide.svg' alt='favory' onclick='ajouterFavoris(" . $row['idM'] . ")'>";
+                        echo "<img src='../res/heartVide.svg' alt='favory' onclick='ajouterFavoris(" . $materiel['idM'] . ")'>";
                     }
                     echo "</button>";
                     echo "</div>";
                     echo "<div class='position-absolute top-0 start-0 ms-3 d-flex justify-content-between align-items-center gap-3 p-4'>";
                         echo "<p id='other' class='text-white rounded p-2 border-0' style='background-color:#e4587d;'>";
-                        echo $row['quantité'] . " " . "disponibles";
+                        echo $materiel['quantité'] . " " . "disponibles";
                         echo "</p>";
                         echo "<p id='phone' class='text-white rounded p-2 border-0' style='background-color:#e4587d;'>";
-                        echo $row['quantité'];
+                        echo $materiel['quantité'];
                         echo "</p>";
                         echo "</div>";
                     // img meme taille que la div
-                    echo "<img src='../materiel/" . $row['photo'] . "' alt='materiel' class='rounded-5 materiel-image'>";
+                    echo "<img src='../materiel/" . $materiel['photo'] . "' alt='materiel' class='rounded-5 materiel-image'>";
                     echo "<div class='d-flex justify-content-center align-items-center flex-column w-100'>";
-                    echo "<a class='text-center fs-auto fw-bold w-100' style='text-decoration:none; color:black;' href='produit.php?id=". $row['idM']. "'>" . $row['designation'] . "</a>";
-                    echo "<button class='btn btn-danger text-white text-center w-80 w-md-50 p-3' onclick='reserverMateriel(" . $row['idM'] . ")'";
+                    echo "<a class='text-center fs-auto fw-bold w-100' style='text-decoration:none; color:black;' href='produit.php?id=". $materiel['idM']. "'>" . $materiel['designation'] . "</a>";
+                    echo "<button class='btn btn-danger text-white text-center w-80 w-md-50 p-3' onclick='reserverMateriel(" . $materiel['idM'] . ")'";
 
-                    if ($row['quantité'] == 0) {
+                    if ($materiel['quantité'] == 0) {
                         echo " disabled";
                     }
 
                     echo ">Réserver</button>";
                     echo "</div>";
                     echo "</div>";
-                }
-                ?>
+                    ?>
+                <?php endforeach; ?>
             </div>
         </section>
     </main>
     <script src="../JS/sideBarre.js"></script>
     <script src="../JS/index.js"></script>
+    <script src="../JS/search_materiel.js"></script>
     <script>
         function reserverMateriel(idM) {
             window.location.href = "reservation_materiel.php?idM=" + idM;
